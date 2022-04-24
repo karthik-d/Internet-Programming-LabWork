b698b1fe9dbe21a5239733edfad9607013380b38
karthik-d <karthikdesingu2000@gmail.com>
2022-04-25 01:23:46 +0530

    login page rendered

--- Ex4_SkillTest_Servlets/WEB-INF/classes/SkillTest/Controller/Login.java
+++ Ex4_SkillTest_Servlets/WEB-INF/classes/SkillTest/Controller/Login.java
@@ -8,7 +8,14 @@ import java.util.*;
 
 public class Login extends HttpServlet{
 
+    /*private String BaseViewsPath = "/WEB-INF/cl"
+
+    private String getViewPath(String relPath){
+        String
+    }*/
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        /*
         response.setContentType("text/html");
         PrintWriter render = response.getWriter();
         render.println("<html>");
@@ -16,5 +23,9 @@ public class Login extends HttpServlet{
         render.println("Hello there!");
         render.println("</body>");
         render.println("</html>");
+        */
+
+        RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/login.html");
+        view.forward(request, response);
     }
 }
