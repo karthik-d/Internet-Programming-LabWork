b1ae1ed915a8ea6e2a9a9e979f748afbdb44a552
karthik-d <karthikdesingu2000@gmail.com>
2022-05-29 16:41:33 +0530

    converted nav to nested react

--- ex7_personal-page_react/src/Components/Header.js
+++ ex7_personal-page_react/src/Components/Header.js
@@ -1,11 +1,64 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 
+
+class Navtab extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        this.props.onSelection(event.target.value);
+    }
+
+    render() {
+        return (
+            <Link to="/" class="Main__navitem" onClick={this.handleClick}>{this.props.tabname}</Link>
+        );
+    }
+}
+
+class Navbar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleTabSelection = this.handleTabSelection.bind(this);
+        this.state = {
+            active_tab: 'home'
+        };
+    }
+
+    handleTabSelection(active_tab) {
+        this.setState({
+            active_tab: active_tab
+        });
+    }
+
+    render() {
+        return (
+            <div id="Main__navbar">
+                <ul class="Main__navLink">
+                    | <Navtab to="/" class="Main__navitem Main__navactive" tabname="HomeTest" />
+                    | <Link to="/" class="Main__navitem Main__navactive">Home</Link>
+                    | <Link to="/tossn" class="Main__navitem">Journey to SSN</Link>
+                    | <Link to="/qualifications" class="Main__navitem">Qualifications</Link>
+                    | <Link to="/interests" class="Main__navitem">Interests</Link>
+                    | <Link to="/events" class="Main__navitem">Events Organized</Link>
+                    | <Link to="/contact" class="Main__navitem">Contact</Link>
+                    |
+                </ul>
+            </div>
+        );
+    }
+}
+
+
 class Header extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { active: false };
     }
 
     render() {
@@ -25,17 +78,7 @@ class Header extends React.Component {
 
                     </div>
                 </header>
-                <div id="Main__navbar">
-                    <ul class="Main__navLink">
-                        | <Link to="/" class="Main__navitem Main__navactive">Home</Link>
-                        | <Link to="/tossn" class="Main__navitem">Journey to SSN</Link>
-                        | <Link to="/qualifications" class="Main__navitem">Qualifications</Link>
-                        | <Link to="/interests" class="Main__navitem">Interests</Link>
-                        | <Link to="/events" class="Main__navitem">Events Organized</Link>
-                        | <Link to="/contact" class="Main__navitem">Contact</Link>
-                        |
-                    </ul>
-                </div>
+                <Navbar />
             </div >
         );
     }
