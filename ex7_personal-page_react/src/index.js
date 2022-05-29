1ca09e32ade0ea518397d49f99f10be8967adf2a
karthik-d <karthikdesingu2000@gmail.com>
2022-05-29 15:47:52 +0530

    home page added

--- ex7_personal-page_react/src/index.js
+++ ex7_personal-page_react/src/index.js
@@ -2,17 +2,14 @@ import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import './index.css';
 import './styles.css';
 import ReactDOM from "react-dom/client";
-import Home from './Pages/Home';
-import Courses from './Pages/Courses';
 
+import Home from './Components/Home';
 import Header from './Components/Header';
 import ToSSN from './Components/ToSSN';
 import Qualifications from './Components/Qualifications';
 import Interests from './Components/Interests';
 import Events from './Components/Events';
 import Contact from './Components/Contact';
-import Search from './Components/Search';
-import List from './Components/List';
 
 function App() {
   return (
