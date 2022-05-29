2618a3cd9fe4056259ccd9a3471d7bcfe689f8d2
karthik-d <karthikdesingu2000@gmail.com>
2022-05-29 16:06:46 +0530

    Convert header to class component

--- ex7_personal-page_react/src/Components/Header.js
+++ ex7_personal-page_react/src/Components/Header.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 
-const Header = () => {
-    return (
-        <div>
-            <header id="Header">
-                <div id="Header__title">
-                    <p id="Header__titleText">
-                        Karthik's Page
-                    </p>
-                </div>
+class Header extends React.Component {
 
-                <div id="Header__content" >
-                    <div class="Header__welcometext">
-                        <p>Hey there! Welcome to my page.</p>
+    constructor(props) {
+        super(props);
+        this.state = { active: false };
+    }
+
+    render() {
+        return (
+            <div>
+                <header id="Header">
+                    <div id="Header__title">
+                        <p id="Header__titleText">
+                            Karthik's Page
+                        </p>
                     </div>
 
+                    <div id="Header__content" >
+                        <div class="Header__welcometext">
+                            <p>Hey there! Welcome to my page.</p>
+                        </div>
+
+                    </div>
+                </header>
+                <div id="Main__navbar">
+                    <ul class="Main__navLink">
+                        | <Link to="/" class="Main__navitem Main__navactive">Home</Link>
+                        | <Link to="/tossn" class="Main__navitem">Journey to SSN</Link>
+                        | <Link to="/qualifications" class="Main__navitem">Qualifications</Link>
+                        | <Link to="/interests" class="Main__navitem">Interests</Link>
+                        | <Link to="/events" class="Main__navitem">Events Organized</Link>
+                        | <Link to="/contact" class="Main__navitem">Contact</Link>
+                        |
+                    </ul>
                 </div>
-            </header>
-            <div id="Main__navbar">
-                <ul class="Main__navLink">
-                    | <Link to="/" class="Main__navitem Main__navactive">Home</Link>
-                    | <Link to="/tossn" class="Main__navitem">Journey to SSN</Link>
-                    | <Link to="/qualifications" class="Main__navitem">Qualifications</Link>
-                    | <Link to="/interests" class="Main__navitem">Interests</Link>
-                    | <Link to="/events" class="Main__navitem">Events Organized</Link>
-                    | <Link to="/contact" class="Main__navitem">Contact</Link>
-                    |
-                </ul>
-            </div>
-        </div >
-    );
+            </div >
+        );
+    }
 }
 
 export default Header;
